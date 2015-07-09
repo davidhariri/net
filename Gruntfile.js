1173aeda23a61413e379460c287a7dfbd883f3c7
David Hariri <david.hariri@gmail.com>
2015-07-09 09:54:06 -0400

    removed ES6 support for build

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,13 @@
 // 2: Convert and move test and source to /temp for ES5 compatibility
 // 3: Run our tests
 // 4: Build source, map, and minified version from ES5 in /temp and from /source (both ES6 and ES5 libraries)
-// 5: Check size against 16kb
-// 6: Clean the /temp and /build dirs
+// 5: Clean the /temp and /build dirs
+
+// Note: Have to transpile all to ES5 for now since uglify doesn't work on ES6 yet
 
 module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
-	// TODO: When ES6 is standard remove Babel
+	// TODO: When ES6 becomes the standard remove Babel and all that jank below
     grunt.initConfig({
 		clean : {
 			default : ['temp/*.js']
@@ -15,15 +16,29 @@ module.exports = function(grunt) {
 		babel : {
 			default : {
 				files: {
-	                'temp/go.es5.js': 'source/go.js',
-					'temp/go.test.es5.js': 'test/go.js',
+	                'temp/go.js': 'source/go.js',
+					'temp/go.test.js': 'test/go.js',
 	            }
 			}
 		},
 		nodeunit : {
-			files : ['temp/go.test.es5.js']
+			files : ['temp/go.test.js']
+		},
+		copy : {
+			default : {
+				files : [
+					{expand: false, src: ['temp/go.js'], dest: 'build/go.js'}
+				]
+			}
+		},
+		uglify : {
+			default: {
+				files: {
+		        	'build/go.min.js': ['build/go.js']
+				}
+		    }
 		}
     });
 
-	grunt.registerTask('default', ['clean', 'babel', 'nodeunit', 'clean']);
+	grunt.registerTask('default', ['clean', 'babel', 'nodeunit', 'copy', 'uglify', 'clean']);
 };
--- test/go.js
+++ test/go.js
@@ -1,4 +1,5 @@
-import * as go from '../temp/go.es5.js'
+// Import the temporary test version of go created in step 1 of grunt
+import * as go from '../temp/go.js'
 
 export function testSetUp(test) {
 	const testSettings = {
@@ -11,7 +12,8 @@ export function testSetUp(test) {
 
 	var setSettings = go.setup(testSettings);
 
-	test.equals(testSettings.headers, setSettings.headers, "Test go.setup with headers");
-	test.equals(testSettings.type, setSettings.type, "Test go.setup with type");
+	test.equals(testSettings.headers, setSettings.headers, "Test go.setup() with headers");
+	test.equals(testSettings.type, setSettings.type, "Test go.setup() with type");
+
 	test.done();
 }
