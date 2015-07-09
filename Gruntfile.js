4d189fdfb364620ae6301d69be9bbcd6cbc0d193
David Hariri <david.hariri@gmail.com>
2015-07-09 10:03:59 -0400

    added some notes to the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,21 @@
+// NOTE: My methodology is "/build/go.js" is always the latest passing build
+// NOTE: I achieve this by making sure all tests pass before putting anything in /build
+// NOTE: I treat /temp as a kind of quarantine
+// NOTE: Below are my instructions:
+
 // 1: Clean the /temp and /build dirs
-// 2: Convert and move test and source to /temp for ES5 compatibility
-// 3: Run our tests
-// 4: Build source, map, and minified version from ES5 in /temp and from /source (both ES6 and ES5 libraries)
+// 2: Transpile /source/go.js and /test/go.js and
+// 3: Run our tests in the temp folder
+// 4: Copy over our library once all tests have passed to /build/go.js
+// 5: Minifiy /build/go.js to /build/go.min.js
 // 5: Clean the /temp and /build dirs
 
-// Note: Have to transpile all to ES5 for now since uglify doesn't work on ES6 yet
+// NOTE: We have to transpile all js to ES5 for now since uglify doesn't work on ES6 yet
+// NOTE: In the future I'd like to be able to build two different versions
+// NOTE: In the future future I'd like to be able to remove all this crap and just have it run natively in ES6 syntax :-)
 
 module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
-	// TODO: When ES6 becomes the standard remove Babel and all that jank below
     grunt.initConfig({
 		clean : {
 			default : ['temp/*.js']
